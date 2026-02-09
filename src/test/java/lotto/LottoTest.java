b2bbf6090ef47fe80f6d882e6e575e859a3edd29
paul.an <paul.an@kakaocorp.com>
2026-02-09 18:22:28 +0900

    test: LottoTest 추가 및 입력 검증 테스트 작성
    - 랜덤/수동 로또 생성 시 번호 6개 생성 여부 검증
    - 입력 개수 부족, 비숫자, 범위 초과, 중복 입력에 대한 예외 검증

---
+++ src/test/java/lotto/LottoTest.java
@@ -0,0 +1,59 @@
+package lotto;
+
+import level0.InputParser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class LottoTest {
+
+    @Test
+    @DisplayName("랜덤 숫자 6개를 포함한 한 장의 로또를 생성한다.")
+    public void generateLottoTest() {
+        Lotto lotto = new Lotto();
+
+        assertNotNull(lotto);
+        assertThat(lotto.getLottoNumbers().size()).isEqualTo(6);    // 로또의 lottoNumbers 사이즈가 6인지 검증
+    }
+
+    @Test
+    @DisplayName("입력받은 숫자 6개를 포함한 한 장의 로또를 생성한다.")
+    public void manualLottoTest() {
+        Lotto lotto = new Lotto("1, 2, 3, 4, 5, 6");
+
+        assertNotNull(lotto);
+        assertThat(lotto.getLottoNumbers().size()).isEqualTo(6);    // 로또의 lottoNumbers 사이즈가 6인지 검증
+    }
+
+    @Test
+    @DisplayName("5개의 숫자만 입력받으면 예외 처리한다.")
+    public void manualLottoFailSizeTest() {
+        RuntimeException runtimeException = Assertions.assertThrows(RuntimeException.class, () -> new Lotto("1, 2, 3, 4, 6"));
+        assertThat(runtimeException.getMessage()).isEqualTo("6개의 숫자를 입력해야 합니다.");
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 값을 입력받으면 예외 처리한다.")
+    public void manualLottoFailNotNumberTest() {
+        RuntimeException runtimeException = Assertions.assertThrows(RuntimeException.class, () -> new Lotto("1, 2, 3, c, 5b, 6a"));
+        assertThat(runtimeException.getMessage()).isEqualTo("숫자가 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("1-45 범위를 벗어난 숫자를 입력받으면 예외 처리한다.")
+    public void manualLottoFailRangeTest() {
+        RuntimeException runtimeException = Assertions.assertThrows(RuntimeException.class, () -> new Lotto("1, 2, 3, 4, 100, 45"));
+        assertThat(runtimeException.getMessage()).isEqualTo("범위를 벗어난 숫자입니다.");
+    }
+
+    @Test
+    @DisplayName("중복된 숫자를 입력받으면 예외 처리한다.")
+    public void manualLottoFailDistinctTest() {
+        RuntimeException runtimeException = Assertions.assertThrows(RuntimeException.class, () -> new Lotto("1, 2, 3, 4, 5, 5"));
+        assertThat(runtimeException.getMessage()).isEqualTo("중복된 숫자입니다.");
+    }
+
+}
