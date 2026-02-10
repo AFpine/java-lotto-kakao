20b421a4d6e35c0e22405ca56a844d1679cd364a
paul.an <paul.an@kakaocorp.com>
2026-02-10 14:17:28 +0900

    feat: LottoGame 기능 추가
    - 당첨 로또 필드 추가 및 생성 기능
    - 로또 단건에 대한 결과 반환 메서드
    - 전체 로또들에 대한 당첨금 총액 반환 메서드
    - 수익률 반환 메서드

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class LottoGame {
 
     // 정답 로또
+    private WinningLotto winningLotto;
 
     // 구매 로또 리스트
     private List<Lotto> lottoList;
@@ -30,5 +31,54 @@ public class LottoGame {
         for (int i = 0; i < count; i++) {
             lottoList.add(new Lotto());
         }
+    }
+
+    // 당첨 로또 생성
+    public void createWinningLotto(String input, String bonus) {
+        winningLotto = new WinningLotto(input);
+        winningLotto.createBonusNumber(bonus);
+    }
+
+    // 하나의 로또의 결과 반환
+    public LottoEnum getOneLottoResult(Lotto lotto) {
+        int matchCount = 0;
+        boolean bonusCount = false;
+        for (LottoNumber lottoNumber : lotto.getLottoNumbers()) {
+            for (LottoNumber winningLottoNumber : winningLotto.getLottoNumbers()) {
+                if(lottoNumber.getNumber() == winningLottoNumber.getNumber()) matchCount += 1;
+            }
+            if(winningLotto.getBonusNumber().getNumber() == lottoNumber.getNumber()) bonusCount = true;
+        }
+
+        // 로또에 enum 설정
+        lotto.setLottoEnum(calculateLottoEnum(matchCount, bonusCount));
+        return lotto.getLottoEnum();
+    }
+
+    // 로또의 결과 Enum 반환
+    public LottoEnum calculateLottoEnum(int matchCount, boolean bonusCount) {
+        if(matchCount == 6) return LottoEnum.FIRST;
+        if(matchCount == 5 && bonusCount) return LottoEnum.SECOND;
+        if(matchCount == 5) return LottoEnum.THIRD;
+        if(matchCount == 4) return LottoEnum.FOURTH;
+        if(matchCount == 3) return LottoEnum.FIFTH;
+        return LottoEnum.OTHER;
+    }
+
+    // 로또 리스트들의 당첨금 총액 반환
+    public int getLottoSum() {
+        int sum = 0;
+        for (Lotto lotto : lottoList) {
+            sum += (int) lotto.getLottoEnum().getValue();
+        }
+
+        return sum;
+    }
+
+    // 수익률 반환
+    public double getRateOfReturn(int sum) {
+        double rate = (double) sum / (lottoList.size() * 1000);
+        // 100을 곱해서 반올림하고 다시 100.0으로 나눔
+        return Math.floor(rate * 100) / 100.0;
     }
 }
