6ec82f671e04e795bbbba31ab2de6002e84225e4
paul.an <paul.an@kakaocorp.com>
2026-02-10 10:27:25 +0900

    test: 로또 게임 테스트 코드 작성
    - 구매 가능 장수 입력 시 해당 개수에 맞게 로또 구매

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -19,14 +19,22 @@ public class LottoGameTest {
 
     @Test
     @DisplayName("로또 구매 금액을 전달하면 구매할 수 있는 로또 장수를 반환한다.")
-    public void purchaseLottoTest() {
-        Assertions.assertThat(lottoGame.purchase(1500)).isEqualTo(1);
+    public void calculateLottoCountTest() {
+        Assertions.assertThat(lottoGame.calculateLottoCount(1500)).isEqualTo(1);
     }
 
     @Test
     @DisplayName("로또 구매 금액이 1000원 미만이면 예외를 반환한다.")
-    public void purchaseLottoExceptionTest() {
-        RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> lottoGame.purchase(500));
+    public void calculateLottoCountExceptionTest() {
+        RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> lottoGame.calculateLottoCount(500));
         assertThat(runtimeException.getMessage()).isEqualTo("1000원 이상의 금액을 입력해야 합니다.");
+    }
+
+    @Test
+    @DisplayName("구매 가능 장수를 넣으면 그에 맞는 사이즈의 로또 리스트를 반환한다.")
+    public void purchaseLottoTest() {
+        lottoGame.purchaseLotto(3);
+
+        assertThat(lottoGame.getLottoList().size()).isEqualTo(3);
     }
 }
