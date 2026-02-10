6210051d27e0da10bd800b5503a1b98dec0ad285
paul.an <paul.an@kakaocorp.com>
2026-02-10 15:22:50 +0900

    refactor: ENUM 클래스에 설명 추가

--- src/main/java/lotto/LottoEnum.java
+++ src/main/java/lotto/LottoEnum.java
@@ -1,18 +1,24 @@
 package lotto;
 
 public enum LottoEnum {
-    FIRST(2_000_000_000),
-    SECOND(30_000_000),
-    THIRD(1_500_000),
-    FOURTH(50_000),
-    FIFTH(5_000),
-    OTHER(0);
+    FIFTH("3개 일치", 5_000),
+    FOURTH("4개 일치",50_000),
+    THIRD("5개 일치",1_500_000),
+    SECOND("5개 일치, 보너스 볼 일치",30_000_000),
+    FIRST("6개 일치",2_000_000_000);
 
+    private final String description;
     private final long value;
 
-    LottoEnum(long value) {
+    LottoEnum(String description, long value) {
+        this.description = description;
         this.value = value;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
     public long getValue() {
         return this.value;
     }
