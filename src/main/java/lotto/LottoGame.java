a1cb36204422b7a840b821184eaf4df0967111e0
paul.an <paul.an@kakaocorp.com>
2026-02-11 11:48:25 +0900

    refactor: (피드백 반영) LottoGame이 너무 많은 책임 가지는 문제 해결
    - 당첨금, 수익률, 결과 생성 로직을 위임

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGame {
 
     // 정답 로또
@@ -25,15 +28,18 @@ public class LottoGame {
 
     // 로또 구매
     public void purchaseLotto(int count) {
-        for (int i = 0; i < count; i++) {
-            lottos.add(new Lotto());
-        }
+        lottos.purchaseLotto(count);
     }
 
-    public void printLottoList() {
+    // 구매한 로또 출력
+    public List<List<Integer>> getLottoListAsList() {
+        List<List<Integer>> list = new ArrayList<>();
+
         for (Lotto lotto : lottos.getLottoList()) {
-            lotto.printLottoNumbers();
+            list.add(lotto.getLottoNumbersAsList());
         }
+
+        return list;
     }
 
     // 당첨 로또 생성
@@ -41,48 +47,8 @@ public class LottoGame {
         winningLotto = new WinningLotto(input, bonus);
     }
 
+    // 모든 로또 결과 설정
     public void setAllLottoResult() {
         lottos.setAllLottoResult(winningLotto);
-    }
-
-    // 로또 리스트들의 당첨금 총액 반환
-    public int getLottoSum() {
-        int sum = 0;
-        for (Lotto lotto : lottos.getLottoList()) {
-            if(lotto.getLottoEnum() != null) sum += (int) lotto.getLottoEnum().getValue();
-        }
-
-        return sum;
-    }
-
-    // 수익률 반환
-    public double getRateOfReturn(int sum) {
-        double rate = (double) sum / (lottos.getLottoList().size() * 1000);
-        // 100을 곱해서 반올림하고 다시 100.0으로 나눔
-        return Math.floor(rate * 100) / 100.0;
-    }
-
-    public int countEnum(LottoEnum lottoEnum) {
-        int count = 0;
-        for (Lotto lotto : lottos.getLottoList()) {
-            if(lotto.getLottoEnum() == lottoEnum) count ++;
-        }
-
-        return count;
-    }
-
-    public void printResult() {
-
-        for (LottoEnum lottoEnum : LottoEnum.values()) {
-            System.out.println(lottoEnum.getDescription() + "(" + lottoEnum.getValue() + "원)- " + countEnum(lottoEnum) + "개");
-        }
-    }
-
-    public void printRateOfReturn() {
-        double rateOfReturn = getRateOfReturn(getLottoSum());
-        System.out.print("총 수익률은 " + rateOfReturn + "입니다.(기준이 1이기 때문에 결과적으로 ");
-
-        if(rateOfReturn < 1.0) System.out.println("손해라는 의미임)");
-        if(rateOfReturn > 1.0) System.out.println("이득이라는 의미임)");
     }
 }
