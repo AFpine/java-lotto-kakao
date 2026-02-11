9b0020e3553d2a017486e6d2a0b9cbd79c7f2ce2
paul.an <paul.an@kakaocorp.com>
2026-02-11 14:37:05 +0900

    test: (피드백 반영) LottoNumberValidator 수정으로 인한 예외 수정

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,8 +28,8 @@ public class LottoGameTest {
     @Test
     @DisplayName("로또 구매 금액이 1000원 미만이면 예외를 반환한다.")
     public void calculateLottoCountExceptionTest() {
-        RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> lottoGame.calculateLottoCount(500));
-        assertThat(runtimeException.getMessage()).isEqualTo("1000원 이상의 금액을 입력해야 합니다.");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> lottoGame.calculateLottoCount(500));
+        assertThat(exception.getMessage()).isEqualTo("1000원 이상의 금액을 입력해야 합니다.");
     }
 
     @Test
@@ -42,11 +44,11 @@ public class LottoGameTest {
     @DisplayName("정답 로또와 구매한 로또 하나를 비교하여 결과 Enum을 반환한다.")
     public void getOneLottoResultTest() {
         lottoGame.createWinningLotto("1, 2, 3, 4, 5, 6", "7");
-        Lotto lotto1 = createFixedLotto(1, 2, 3, 4, 5, 6);
-        Lotto lotto2 = createFixedLotto(1, 2, 3, 4, 5, 7);
-        Lotto lotto3 = createFixedLotto(1, 2, 3, 4, 5, 8);
-        Lotto lotto4 = createFixedLotto(1, 2, 3, 4, 9, 8);
-        Lotto lotto5 = createFixedLotto(1, 2, 3, 10, 9, 8);
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        Lotto lotto3 = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+        Lotto lotto4 = new Lotto(List.of(1, 2, 3, 4, 9, 8));
+        Lotto lotto5 = new Lotto(List.of(1, 2, 3, 10, 9, 8));
 
         lotto1.setOneLottoResult(new WinningLotto("1, 2, 3, 4, 5, 6", "7"));
         lotto2.setOneLottoResult(new WinningLotto("1, 2, 3, 4, 5, 6", "7"));
@@ -59,42 +61,5 @@ public class LottoGameTest {
         assertThat(lotto3.getLottoEnum()).isEqualTo(LottoEnum.THIRD);
         assertThat(lotto4.getLottoEnum()).isEqualTo(LottoEnum.FOURTH);
         assertThat(lotto5.getLottoEnum()).isEqualTo(LottoEnum.FIFTH);
-    }
-
-    @Test
-    @DisplayName("당첨 결과를 입력하면 당첨금 총액을 반환한다.")
-    public void getLottoSumTest() {
-        lottoGame.purchaseLotto(2);
-        lottoGame.getLottos().getLottoList().get(0).setLottoEnum(LottoEnum.FIRST);
-        lottoGame.getLottos().getLottoList().get(1).setLottoEnum(LottoEnum.THIRD);
-
-        assertThat(lottoGame.getLottoSum()).isEqualTo(2_001_500_000);
-    }
-
-    @Test
-    @DisplayName("당첨 금액과 구매 수량을 넣으면 수익률을 반환한다.")
-    public void getRateOfReturn() {
-        int winning = 1_500_000;       // 당첨금 150만원
-        lottoGame.purchaseLotto(3); // 로또 3장 구매
-
-        assertThat(lottoGame.getRateOfReturn(winning)).isEqualTo(500.0);
-    }
-
-    @Test
-    @DisplayName("당첨 금액과 구매 수량을 넣으면 수익률을 반환한다. (예시 데이터와 동일)")
-    public void getRateOfReturn1() {
-        int winning = 5_000;       // 당첨금 150만원
-        lottoGame.purchaseLotto(14); // 로또 3장 구매
-
-        assertThat(lottoGame.getRateOfReturn(winning)).isEqualTo(0.35);
-    }
-
-    private Lotto createFixedLotto(int... numbers) {
-        Lotto lotto = new Lotto();
-        lotto.getLottoNumbers().getLottoNumberList().clear();
-        for (int number : numbers) {
-            lotto.getLottoNumbers().getLottoNumberList().add(new LottoNumber(number));
-        }
-        return lotto;
     }
 }
