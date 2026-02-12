9865f9e95df7fa276c5612dc47a2472a40697645
afpine <afpine527@gmail.com>
2026-02-12 16:17:17 +0900

    refactor: WinningLottoNumbers 생성자에서 로또 번호 중복 검증

--- src/main/java/lotto/WinningLottoNumbers.java
+++ src/main/java/lotto/WinningLottoNumbers.java
@@ -3,31 +3,31 @@ package lotto;
 import java.util.ArrayList;
 import java.util.List;
 
-import static lotto.LottoNumberValidator.*;
-
 public class WinningLottoNumbers {
 
     private List<LottoNumber> lottoNumberList;
 
 
+    // parsing은 parser가 맡아서 한다. 여기선 생성 + validation
+    public WinningLottoNumbers(List<Integer> numbers) {
+        validateSize(numbers);
+        lottoNumberList = new ArrayList<>();
+
+        for (Integer number : numbers) {
+            validateDistinctNumber(lottoNumberList, number);
+            lottoNumberList.add(LottoNumber.from(number));
+        }
+    }
+
     public List<LottoNumber> getLottoNumberList() {
         return lottoNumberList;
     }
 
-    public WinningLottoNumbers(String input) {
-        parseLottoNumbers(input);
+    public void validateDistinctNumber(List<LottoNumber> lottoNumberList, Integer number) {
+        if(lottoNumberList.contains(LottoNumber.from(number))) throw new IllegalArgumentException("로또에 중복된 숫자가 존재합니다.");
     }
 
-    private void parseLottoNumbers(String input) {
-        lottoNumberList = new ArrayList<>();
-
-        String[] strings = input.split(", ");
-        validateSize(strings);
-        for (String s : strings) {
-            validateString(s);
-            validateRange(Integer.parseInt(s));
-            validateDistinctNumber(this.lottoNumberList, Integer.parseInt(s));
-            lottoNumberList.add(LottoNumber.from(Integer.parseInt(s)));
-        }
+    public void validateSize(List<Integer> numbers) {
+        if(numbers.size() != 6) throw new IllegalArgumentException("6개의 숫자를 입력해야 합니다.");
     }
 }
