a724b7b3cd84cae7a785871e28ea7b3ac6f34461
afpine <afpine527@gmail.com>
2026-02-12 12:24:29 +0900

    refactor: LottoNumber static 하게 미리 생성하고 캐싱해서 사용

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -4,6 +4,15 @@ import java.util.Objects;
 
 public class LottoNumber {
     private final int number;
+    private static final int MIN = 1;
+    private static final int MAX = 45;
+    private static final LottoNumber[] CACHE = new LottoNumber[MAX + 1];
+
+    static {
+        for (int i = MIN; i <= MAX; i++) {
+            CACHE[i] = new LottoNumber(i);
+        }
+    }
 
     private LottoNumber(int number) {
         this.number = number;
@@ -11,7 +20,7 @@ public class LottoNumber {
 
     public static LottoNumber from(int number) {
         LottoNumberValidator.validateRange(number);
-        return new LottoNumber(number);
+        return CACHE[number];
     }
 
     public int getNumber() {
