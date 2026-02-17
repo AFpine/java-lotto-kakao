07e39d96dea910416a6117847def779cb04931ce
afpine <afpine527@gmail.com>
2026-02-17 22:36:55 +0900

    feat: 수동으로 구매할 로또 수를 입력받는 printManualLottoPurchaseAmountInput 메서드 추가, 구매 개수 출력메서드 printPurchaseAmount 수정

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -8,8 +8,12 @@ public class OutputView {
         System.out.println("구입 금액을 입력해 주세요.");
     }
 
-    public void printPurchaseAmount(int lottoCount) {
-        System.out.println(lottoCount + "개를 구매했습니다");
+    public void printManualLottoPurchaseAmountInput() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+    }
+
+    public void printPurchaseAmount(int manualLottoCount, int totalLottoCount) {
+        System.out.println("수동으로" + manualLottoCount + "장, 자동으로" + (totalLottoCount-manualLottoCount) + "개를 구매했습니다");
     }
 
     public void printWinningLottoInput() {
