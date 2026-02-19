7e8f976769bbcb21581a40ddbe3c4f253788d44f
afpine <afpine527@gmail.com>
2026-02-19 13:42:05 +0900

    refactor: Lottos에서 관리하던 결과 통계 메서드 삭제

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -21,37 +21,10 @@ public class Lottos {
         this.lottoList.add(lotto);
     }
 
-    public void setAllLottoResult(WinningLotto winningLotto) {
+    public void setAllLottoResult(GameResult gameResult, WinningLotto winningLotto) {
         for (Lotto lotto : lottoList) {
-            lotto.evaluateRank(winningLotto);
+            gameResult.increaseOne(lotto.evaluateRank(winningLotto));
         }
-    }
-
-    // 로또 리스트들의 당첨금 총액 반환
-    public long getLottoSum() {
-        long sum = 0;
-        for (Lotto lotto : lottoList) {
-            sum += lotto.getLottoRankValue();
-        }
-
-        return sum;
-    }
-
-    // 수익률 반환
-    public double getRateOfReturn(long sum) {
-        double rate = (double) sum / (lottoList.size() * LottoPolicy.LOTTO_PRICE);
-        // 100을 곱해서 반올림하고 다시 100.0으로 나눔
-        return Math.floor(rate * 100) / 100.0;
-    }
-
-    // 특정 Enum(몇개 당첨인지) 개수 카운트
-    public int countEnum(LottoRank lottoRank) {
-        int count = 0;
-        for (Lotto lotto : lottoList) {
-            if(lotto.getLottoRank() == lottoRank) count ++;
-        }
-
-        return count;
     }
 
     public void purchaseOneManualLotto(String input) {
